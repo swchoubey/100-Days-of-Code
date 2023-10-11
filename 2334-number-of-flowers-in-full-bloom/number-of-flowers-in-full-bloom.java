98a55fd8540f4a495497f33616226a85df109088
swchoubey <97143500+swchoubey@users.noreply.github.com>
2023-10-11 22:40:28 +0530

    Time: 38 ms (89.62%) | Memory: 72.4 MB (55.39%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -1,22 +1,51 @@
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        TreeMap<Integer,Integer> blooming = new TreeMap<>();
-        int[] arr=new int[people.length];
-        int count=0;
-        for(int i=0;i<flowers.length;i++){
-            int start=flowers[i][0];
-            int end=flowers[i][1];
-            blooming.put(start,blooming.getOrDefault(start,0)+1);
-            blooming.put(end+1,blooming.getOrDefault(end+1,0)-1);
+        int n=flowers.length;
+        int m=people.length;
+        int[] beginBloom=new int[n];
+        int[] stopBloom=new int[n];
+        for(int i=0;i<n;i++){
+            beginBloom[i]=flowers[i][0];
+            stopBloom[i]=flowers[i][1];
         }
-        for(int key:blooming.keySet()){
-            count+=blooming.get(key);
-            blooming.put(key,count);
-        }
-        for(int i=0;i<people.length;i++){
-            Integer key=blooming.floorKey(people[i]);
-            arr[i]=(key==null)?0:blooming.get(key);
+        Arrays.sort(beginBloom);
+        Arrays.sort(stopBloom);
+        int[] arr=new int[m];
+        for(int i=0;i<m;i++){
+            int target=people[i];
+            int open=binarySearchUpper(beginBloom,target);
+            int close=binarySearchLower(stopBloom,target);
+            arr[i]=open-close;
         }
         return arr;
     }
+    private int binarySearchUpper(int[] arr, int target){
+        int left=0;
+        int right=arr.length;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]<=target){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+    private int binarySearchLower(int[] arr, int target){
+        int left=0;
+        int right=arr.length;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+
 }
