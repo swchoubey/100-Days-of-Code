d21ef745c2023782614045cca9a5f85be156ad63
swchoubey <97143500+swchoubey@users.noreply.github.com>
2023-10-23 22:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (58.82%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1)
+        //Original solution
+       /* if(n==1)
             return true;
         if(n<=0)
             return false;
         double x=Math.log(n)/Math.log(4);
 
-        return (x==(int)x);
+        return (x==(int)x);*/
+        return((n>0) && ((n&(n-1))==0) && (n%3==1));
     }
 }
