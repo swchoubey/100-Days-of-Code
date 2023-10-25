a890092438f58bf21636ab80eb79cbd8ad1b3b6c
swchoubey <97143500+swchoubey@users.noreply.github.com>
2023-10-25 18:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 38.4 MB (98.69%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -3,7 +3,8 @@ class Solution {
         if(n==1){
             return 0;
         }
-        int len=1<<(n-1); //Calculating the length of the prev row
+        //Each row has double the number of elements as the previous row, for ex: row 1 has 1 element[0], row 2 has 2 elements[0 1], row 3 has 4 elements [0 1 1 0] and so on.The pattern here indicates that the length of the nth row will be 2^(n-1). Since each bitwise left shift multiplies a number by 2, 1<<(n-1) shifts the binary representation of 1 by (n-1) bits
+        int len=1<<(n-1); 
         if(k<=len/2){
             return kthGrammar(n-1,k);
         }
