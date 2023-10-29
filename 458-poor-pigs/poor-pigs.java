cd897991322c5e7a68d40f088e6425c1ea39bbf1
swchoubey <97143500+swchoubey@users.noreply.github.com>
2023-10-29 23:27:22 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (49.09%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int tests = minutesToTest/minutesToDie + 1;
+        int pigs = 0;
+        while(Math.pow(tests,pigs)<buckets){
+            pigs++;
+        }
+        return pigs;
+    }
+}
