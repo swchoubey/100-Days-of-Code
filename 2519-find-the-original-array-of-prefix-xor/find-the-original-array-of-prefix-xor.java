84d1a93c922b4c500c0e592faa964abe7348b6c7
swchoubey <97143500+swchoubey@users.noreply.github.com>
2023-10-31 23:17:03 +0530

    Time: 1 ms (100.00%) | Memory: 60.5 MB (20.13%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.java
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.java
@@ -1,11 +1,9 @@
 class Solution {
     
     public int[] findArray(int[] pref) {
-        int[] xor = new int[pref.length];
-        xor[0] = pref[0];
-            for(int i=1; i<pref.length; i++){
-                xor[i] = pref[i-1] ^ pref[i];
+            for(int i=pref.length-1; i>0; i--){
+                pref[i] = pref[i-1] ^ pref[i];
         }
-        return xor;
+        return pref;
     }
 }
