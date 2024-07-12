4ba8fc75b833f91c2ad5890b1e25770d6740b5cb
swchoubey <97143500+swchoubey@users.noreply.github.com>
2024-07-13 02:27:24 +0530

    Time: 1 ms (98.83%) | Memory: 41.9 MB (61.56%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int res = 0;
+        for(String log : logs){
+            if(log.equals("../")){
+                if(res > 0){res--;}
+            }
+            else if(!log.equals("./")){
+                res++;
+            }
+        }
+        return res;
+    }
+}
